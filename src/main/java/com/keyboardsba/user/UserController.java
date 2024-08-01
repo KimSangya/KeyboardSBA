db0bced231f6d3fbbf142962832b2495a499dce4
DESKTOP-2TMEOMQ\user <gunhok99@gmail.com>
2024-08-01 18:44:30 +0900

    2024/08/01 초기 HTML 설정

---
+++ src/main/java/com/keyboardsba/user/UserController.java
@@ -0,0 +1,15 @@
+package com.keyboardsba.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/user")
+@Controller
+public class UserController {
+	
+	@GetMapping("/sign-up-view")
+	public String signUp() {
+		return "user/signUp";
+	}
+}
