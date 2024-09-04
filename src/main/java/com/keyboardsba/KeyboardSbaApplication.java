24b2dd2a6893de9a01c2904cca7c798422be271f
seolmeow <gunhok99@gmail.com>
2024-09-04 17:30:28 +0900

    KimSangya/KeyboardSBA #7 AWS 시작

--- src/main/java/com/keyboardsba/KeyboardSbaApplication.java
+++ src/main/java/com/keyboardsba/KeyboardSbaApplication.java
@@ -2,9 +2,10 @@ package com.keyboardsba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class KeyboardSbaApplication {
+public class KeyboardSbaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KeyboardSbaApplication.class, args);
